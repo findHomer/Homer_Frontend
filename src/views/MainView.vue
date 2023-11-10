f61f3fda55c4c3153b1135194a222b8f1f154c05
Baek Seungyun <fangdol888@naver.com>
2023-11-10 18:03:04 +0900

    feat : add kakao api

---
+++ src/views/MainView.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { ref } from "vue";
+import HeadingNavbar from "../components/icons/layout/HeadingNavbar.vue";
+import KakaoMapComponent from "@/components/KakaoMapComponent.vue";
+
+const cards = ["Today", "Yesterday"];
+const links = [
+  ["mdi-inbox-arrow-down", "Inbox"],
+  ["mdi-send", "Send"],
+  ["mdi-delete", "Trash"],
+  ["mdi-alert-octagon", "Spam"],
+];
+
+const drawer = ref(null);
+</script>
+
+<script>
+export default {
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    drawer: null,
+    links: [
+      ["mdi-inbox-arrow-down", "Inbox"],
+      ["mdi-send", "Send"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+  }),
+};
+</script>
+
+<template>
+  <v-app id="inspire">
+    <HeadingNavbar></HeadingNavbar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet color="grey-lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <KakaoMapComponent />
+    <!-- <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <v-row>
+          <v-col v-for="card in cards" :key="card" cols="12">
+            <v-card>
+              <v-list lines="two">
+                <v-list-subheader :title="card"></v-list-subheader>
+
+                <template v-for="n in 6" :key="n">
+                  <v-list-item>
+                    <template v-slot:prepend>
+                      <v-avatar color="grey-darken-1"></v-avatar>
+                    </template>
+
+                    <v-list-item-title :title="`Message ${n}`"></v-list-item-title>
+
+                    <v-list-item-subtitle
+                      title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique"
+                    ></v-list-item-subtitle>
+                  </v-list-item>
+
+                  <v-divider v-if="n !== 6" :key="`divider-${n}`" inset></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main> -->
+  </v-app>
+</template>
