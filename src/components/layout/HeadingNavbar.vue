6af1d256ea415f8ddfbcac5f61143f92bb0d847b
fangdol888 <fangdol888@gmail.com>
2023-11-15 17:41:32 +0900

    refactor : 코드 컴포넌트로 분리

--- src/components/layout/HeadingNavbar.vue
+++ src/components/layout/HeadingNavbar.vue
@@ -1,30 +1,21 @@
 <script setup>
-import { ref } from "vue";
-import LoginFormModal from "../LoginFormModal.vue";
-
-const links = ref(["Dashboard", "Messages", "Profile", "Updates"]);
+import LoginFormModal from "../user/LoginFormModal.vue";
+import RegisterFormModal from "../user/RegisterFormModal.vue";
 </script>
 
 <template>
   <v-app-bar flat>
     <v-container class="mx-auto d-flex align-center justify-center">
       <v-avatar class="me-4" color="grey-darken-1" size="32"></v-avatar>
-
-      <v-btn v-for="link in links" :key="link" :text="link" variant="text"></v-btn>
+      <v-text>Homer</v-text>
+      <!-- <v-btn v-for="link in links" :key="link" :text="link" variant="text"></v-btn> -->
 
       <v-spacer></v-spacer>
-
-      <v-responsive max-width="160">
-        <!-- <v-text-field
-          density="compact"
-          flat
-          hide-details
-          label="Search"
-          rounded="lg"
-          single-line
-          variant="solo-filled"
-        ></v-text-field> -->
+      <v-responsive max-width="80">
         <LoginFormModal />
+      </v-responsive>
+      <v-responsive max-width="80">
+        <RegisterFormModal />
       </v-responsive>
     </v-container>
   </v-app-bar>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,59 +1,13 @@
 <script setup>
-import { ref } from "vue";
 import HeadingNavbar from "../components/layout/HeadingNavbar.vue";
 import KakaoMapComponent from "@/components/KakaoMapComponent.vue";
-
-const cards = ["Today", "Yesterday"];
-const links = [
-  ["mdi-inbox-arrow-down", "Inbox"],
-  ["mdi-send", "Send"],
-  ["mdi-delete", "Trash"],
-  ["mdi-alert-octagon", "Spam"],
-];
-
-const drawer = ref(null);
-</script>
-
-<script>
-export default {
-  data: () => ({
-    cards: ["Today", "Yesterday"],
-    drawer: null,
-    links: [
-      ["mdi-inbox-arrow-down", "Inbox"],
-      ["mdi-send", "Send"],
-      ["mdi-delete", "Trash"],
-      ["mdi-alert-octagon", "Spam"],
-    ],
-  }),
-};
+import NavigationDrawer from "../components/layout/NavigationDrawer.vue";
 </script>
 
 <template>
   <v-app id="inspire">
-    <HeadingNavbar></HeadingNavbar>
-
-    <v-navigation-drawer v-model="drawer">
-      <v-sheet color="grey-lighten-4" class="pa-4">
-        <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
-
-        <div>john@google.com</div>
-      </v-sheet>
-
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          :prepend-icon="icon"
-          :title="text"
-          link
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    <HeadingNavbar />
+    <NavigationDrawer />
     <KakaoMapComponent />
- 
   </v-app>
 </template>
