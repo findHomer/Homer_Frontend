08bc833bf502d5eaed51d7f301bb49716394a6b7
fangdol888 <fangdol888@gmail.com>
2023-11-16 22:48:14 +0900

    feat : chat room list item 기본 구조 완료

--- src/components/chat/ChatRoomListItemComponent.vue
+++ src/components/chat/ChatRoomListItemComponent.vue
@@ -1,18 +1,36 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  imgSrc: String,
+  name: String,
+  lastUpdated: String,
+  lastChatter: String,
+  lastChat: String,
+});
+</script>
 
 <template>
   <v-container fluid>
     <v-row>
       <v-col cols="3" class="d-flex justify-center">
-        <!-- 사용자 프로필 -->
+        <!-- 채팅방 프로필 -->
         <v-avatar :size="avatarSize" color="red">
           <template v-if="props.imgSrc == null || props.imgSrc == undefined">
             <span class="text-h5">{{ props.userName.substring(0, 1) }}</span>
           </template>
           <template v-else>
-            <v-img :src="props.imgSrc" :alt="props.userName" />
+            <v-img :src="props.imgSrc" :alt="props.name" />
           </template>
         </v-avatar>
+      </v-col>
+      <!-- 채팅방 이름, 최근 보낸 시간, 최근 채팅 기록  -->
+      <v-col>
+        <v-row>
+          <v-col cols="10">{{ props.name }}</v-col>
+          <v-col cols="1">{{ props.lastUpdated }}</v-col>
+        </v-row>
+        <v-row>
+          <v-col>{{ props.lastChatter }} : {{ props.lastChat }}</v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
