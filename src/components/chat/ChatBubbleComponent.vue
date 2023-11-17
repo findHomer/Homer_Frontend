32f6c41a184f94b834a9315c3e9352c80a50979d
fangdol888 <fangdol888@gmail.com>
2023-11-17 16:52:18 +0900

    refactor : chat bubble me component로 이름 변경

--- src/components/chat/ChatBubbleComponent.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-// 내가 쓴 말풍선 컴포넌트
-</script>
-
-<template>
-  <div></div>
-</template>
-
-<style scoped></style>
---
+++ src/components/chat/ChatBubbleMeComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+// 내가 쓴 말풍선 컴포넌트
+</script>
+
+<template>
+  <div>
+    <v-row>
+      <v-spacer cols="1"></v-spacer>
+      <!-- 내용 -->
+      <v-col>
+        <span class="chat-bubble">
+          {{ props.content }}
+        </span>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<style scoped>
+.chat-bubble {
+  border: 1px lightgray solid;
+  border-radius: 1rem;
+  background-color: rgb(213, 235, 255);
+}
+</style>
