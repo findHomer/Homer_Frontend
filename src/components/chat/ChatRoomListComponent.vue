22dfa2fdb143dd833e6bf21d950587d63218de97
fangdol888 <fangdol888@gmail.com>
2023-11-18 12:54:32 +0900

    refactor : 대화방 세부 데이터 추가 및 제목 추가

--- src/components/chat/ChatRoomListComponent.vue
+++ src/components/chat/ChatRoomListComponent.vue
@@ -3,8 +3,22 @@ import ChatRoomListItemComponent from "./ChatRoomListItemComponent.vue";
 
 // mock for test
 const chatrooms = [
-  { id: 1, name: "SSAFY 멀티캠퍼스 역삼점" },
-  { id: 2, name: "SSAFY 멀티캠퍼스 역삼점" },
+  {
+    id: 1,
+    name: "SSAFY 멀티캠퍼스 역삼점",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+    lastUpdated: "2023-04-01 15:23:12",
+    lastChat: "안녕하세요!! ^^",
+    lastChatter: "백빵돌이",
+  },
+  {
+    id: 2,
+    name: "SSAFY 멀티캠퍼스 역삼점",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+    lastUpdated: "2023-04-01 15:23:12",
+    lastChat: "안녕하세요!! ^^",
+    lastChatter: "백빵돌이",
+  },
 ];
 
 const emit = defineEmits(["enter"]);
@@ -16,12 +30,19 @@ const enter = (id) => {
 </script>
 
 <template>
-  <div>
+  <v-container fluid>
+    <v-row>
+      <v-col class="d-flex justify-center">채팅방 목록</v-col>
+    </v-row>
+
     <template v-for="chatroom in chatrooms" :key="chatroom.id">
-      <ChatRoomListItemComponent v-bind="chatroom" @click="enter(chatroom.id)" />
+      <v-row>
+        <v-col><ChatRoomListItemComponent v-bind="chatroom" @click="enter(chatroom.id)" /> </v-col>
+      </v-row>
       <v-divider></v-divider>
     </template>
-  </div>
+  </v-container>
+  <div></div>
 </template>
 
 <style scoped></style>
