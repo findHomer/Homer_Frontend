af2bbed0ad3dbaaad1df5c04ef7c329d02893bde
fangdol888 <fangdol888@gmail.com>
2023-11-18 12:55:09 +0900

    feat : 버튼 눌렀을 때 대화방 열리도록 구현

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,10 +1,18 @@
 <script setup>
 import { ref, watch } from "vue";
 
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 const select = ref(false);
 const chatFab = ref({
   icon: "mdi-chat",
-  color: "black",
+  color: "white",
+});
+
+const btnClass = ref({
+  "chat-btn": true,
+  "px-0": false,
 });
 
 const selection = () => {
@@ -14,34 +22,47 @@ const selection = () => {
 watch(select, () => {
   if (select.value) {
     // dark
-    chatFab.value.color = "white"; // 아이콘 색깔
-  } else chatFab.value.color = "black"; // light 테마
+    chatFab.value.color = "black"; // 아이콘 색깔
+    btnClass.value["px-0"] = true;
+    // 채팅방을 열어라
+    router.push("/chat/chatrooms");
+  } else {
+    chatFab.value.color = "white"; // light 테마
+    btnClass.value["px-0"] = false;
+  }
 });
 </script>
 
 <template>
-  <div>
-    <template>
+  <v-container class="h-100">
+    <v-row>
       <v-fab-transition>
         <v-btn
           :key="chatFab.icon"
           :color="chatFab.color"
-          large
           :dark="select"
           bottom
           right
-          class="chat-btn"
+          :class="btnClass"
           @click="selection"
         >
-          <v-icon>{{ chatFab.icon }}</v-icon>
-          <!-- 아이콘을 아직 선택 안했으므로 -->
-          <template v-if="!select"> 현재 관심있는 아파트에 대한 채팅을 해보실래요? </template>
+          <v-row>
+            <v-col>
+              <v-icon>{{ chatFab.icon }}</v-icon>
+            </v-col>
+            <!-- 아이콘을 아직 선택 안했으므로 -->
+            <template v-if="!select">
+              <v-col> 현재 관심있는 아파트에 대한 채팅을 해보실래요?</v-col>
+            </template>
+          </v-row>
         </v-btn>
       </v-fab-transition>
       <!-- 채팅방 파트 -->
-      <router-view></router-view>
-    </template>
-  </div>
+      <template v-if="select">
+        <router-view></router-view>
+      </template>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
