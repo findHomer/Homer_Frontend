d8f8eff0a5280aa7f4f4e3a9228e39968c4a189f
fangdol888 <fangdol888@gmail.com>
2023-11-19 03:00:39 +0900

    feat  : chatroom inside 기본 구조 및 목업 작성

---
+++ src/components/chat/ChatInputComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from 'vue'
+const message = ref("");
+
+</script>
+
+<template>
+    <v-container >
+        <v-row>
+            <v-col cols="9" class="d-flex align-end">
+                <v-textarea auto-grow variant="outlined" v-model="message" rows=1  max-rows=3 clearable class="w-100" placeholder="채팅을 입력해주세요...">  </v-textarea>
+            </v-col>
+            <v-col class="d-flex align-center">
+                <v-btn  color="success">
+                    <v-icon>mdi-send</v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<style scoped>
+
+</style>
