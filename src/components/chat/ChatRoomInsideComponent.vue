92f0929930a4c86eb2bccc5694f4832e36807b2d
fangdol888 <fangdol888@gmail.com>
2023-11-19 16:58:20 +0900

    feat : 무한 스크롤 api mockup 추가

--- src/components/chat/ChatRoomInsideComponent.vue
+++ src/components/chat/ChatRoomInsideComponent.vue
@@ -48,22 +48,96 @@ chats.value = [
     content: "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
     profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
   },
+
+  {
+    chatId: 5,
+    userId: 2,
+    name: "arceon",
+    content: "안녕하세요!!",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+  },
+  {
+    chatId: 6,
+    userId: 3,
+    name: "헬로월드",
+    content: "안녕하세요!!",
+    profileUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    chatId: 7,
+    userId: 2,
+    name: "신예지",
+    content: "무엇이 궁금하신가요?",
+    profileUrl:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    chatId: 8,
+    userId: 1,
+    name: "백빵돌이",
+    content: "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+  },
+
 ];
 
-const load = async ({done}) => {
+const mockMoreChat=[
+  {
+    chatId: 9,
+    userId: 2,
+    name: "arceon",
+    content: "안녕하세요!!",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+  },
+  {
+    chatId: 10,
+    userId: 3,
+    name: "헬로월드",
+    content: "안녕하세요!!",
+    profileUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    chatId: 11,
+    userId: 2,
+    name: "신예지",
+    content: "무엇이 궁금하신가요?",
+    profileUrl:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    chatId: 12,
+    userId: 1,
+    name: "백빵돌이",
+    content: "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+  },
+]
+
+const api = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockMoreChat);
+    }, 1000);
+  });
+};
+
+const load = async ({ done }) => {
   // TODO : 메세지 불러오기 로직 시간 내림차순으로
 
-  const data = []
+  const data = await api();
+
   //데이터가 없으면
-  if(data == null || data == undefined){
-    done('error')
+  if (data == null || data == undefined) {
+    done("error");
+  } else if (data.length <= 0) {
+    done("empty");
+  } else {
+    chats.value.push(...data)
+    done("ok");
   }
-  else if(data.length <= 0){
-    done('empty')
-  }else {
-    done("ok")
-  }
-}
+};
 </script>
 
 <template>
@@ -82,18 +156,24 @@ const load = async ({done}) => {
       </v-col>
     </v-row>
 
-    <v-infinite-scroll :height="350" :items="chats" :onLoad="load" side="start" empty-text="메세지를 모두 확인했습니다.">
-    <template v-for="chat in chats" :key="chat.chatId">
-      <v-list-item v-if="chat.userId == userId">
-        <ChatBubbleMeComponent v-bind="chat" />
-      </v-list-item>
-      <v-list-item v-else :prepend-avatar="chat.profileUrl" :title="chat.name">
-        <ChatBubbleOthersComponent v-bind="chat" />
-      </v-list-item>
-    </template>
+    <v-infinite-scroll
+      :height="350"
+      :items="chats"
+      :onLoad="load"
+      side="start"
+      empty-text="메세지를 모두 확인했습니다."
+    >
+      <template v-for="chat in chats" :key="chat.chatId">
+        <v-list-item v-if="chat.userId == userId">
+          <ChatBubbleMeComponent v-bind="chat" />
+        </v-list-item>
+        <v-list-item v-else :prepend-avatar="chat.profileUrl" :title="chat.name">
+          <ChatBubbleOthersComponent v-bind="chat" />
+        </v-list-item>
+      </template>
     </v-infinite-scroll>
 
-    <ChatInputComponent bottom/>
+    <ChatInputComponent bottom />
   </v-container>
 </template>
 
--- src/components/chat/ChatRoomListComponent.vue
+++ src/components/chat/ChatRoomListComponent.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { useChatRoomStore } from "@/stores/chatroom";
-
+import {ref} from 'vue'
 const store = useChatRoomStore();
 const { enterRoom } = store;
 
 // mock for test
-const chatrooms = [
+const chatrooms = ref([
   {
     id: 1,
     name: "SSAFY 멀티캠퍼스 역삼점",
@@ -69,18 +69,58 @@ const chatrooms = [
       "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
     lastChatter: "백빵돌이",
   },
-];
+]);
+
+const mockMoreChat=[
+  {
+    id: 5,
+    name: "SSAFY 멀티캠퍼스 역삼점5",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+    lastUpdated: "2023-04-01 15:23:12",
+    lastChat:
+      "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
+    lastChatter: "백빵돌이",
+  },
+  {
+    id: 6,
+    name: "SSAFY 멀티캠퍼스 역삼점6",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+    lastUpdated: "2023-04-01 15:23:12",
+    lastChat:
+      "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
+    lastChatter: "백빵돌이",
+  },
+  {
+    id: 7,
+    name: "SSAFY 멀티캠퍼스 역삼점7",
+    profileUrl: "https://www.ssafy.com/swp/rps/images/campus_seoul.png",
+    lastUpdated: "2023-04-01 15:23:12",
+    lastChat:
+      "안녕하세요!! ^^ 서울에서 저희 본가까지 거리가 긴데 시간이 얼마나 걸릴까요?",
+    lastChatter: "백빵돌이",
+  },
+]
+
+const api = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockMoreChat);
+    }, 1000);
+  });
+};
 
 const load = async ({ done }) => {
   // TODO : 메세지 불러오기 로직 시간 내림차순으로
 
-  const data = [];
+  const data = await api();
+
   //데이터가 없으면
   if (data == null || data == undefined) {
     done("error");
   } else if (data.length <= 0) {
     done("empty");
   } else {
+    chatrooms.value.push(...data)
     done("ok");
   }
 };
