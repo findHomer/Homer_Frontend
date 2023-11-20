d1476f8858ae98a541394d3d9c5c44fa9ced9b6a
hanyj0104 <hanyj0104@naver.com>
2023-11-20 15:20:16 +0900

    feat: 지도 클러스터링 추가 및 렌더링 속도 개선

--- src/components/map/KakaoMapComponent.vue
+++ src/components/map/KakaoMapComponent.vue
@@ -13,51 +13,72 @@ const initMap = async() => {
   const container = document.getElementById("map");
   const options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 5,
+    level: 3,
   }; //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
   const map = new kakao.maps.Map(container, options);
-  kakao.maps.event.addListener(map, 'tilesloaded', async()=> {             
-    for(var i =0 ;i<markers.length;i++){
-      markers[i].setMap(null);
+  var clusterer = new kakao.maps.MarkerClusterer({
+          map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+          averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+          minLevel: 4, // 클러스터 할 최소 지도 레벨 
+          gridSize: 150, //클러스터에 포함될 격자크기    
+          });
+  kakao.maps.event.addListener(map, 'tilesloaded', async () => {
+
+
+    
+
+    //markers.splice(0, markers.length)
+   
+    if (map.getLevel() >= 7) {
+      clusterer.clear();
+      return;
     }
-    markers.splice(0,markers.length)
-    markerMapping.clear();
+
     // 지도 영역정보를 얻어옵니다 
     var bounds = map.getBounds();
-   // console.log(bounds.ha)
-    // 영역정보의 남서쪽 정보를 얻어옵니다 
-    var swLatlng = bounds.getSouthWest();
     
-    // 영역정보의 북동쪽 정보를 얻어옵니다 
-    var neLatlng = bounds.getNorthEast();
     const response = await axios.get(`/apartments/locations/maps?startLat=${bounds.qa}&endLat=${bounds.pa}&startLng=${bounds.ha}&endLng=${bounds.oa}`)
-    //console.log(response);
-      response.data.forEach(element => {
-        var marker = new kakao.maps.Marker({
-          position : new kakao.maps.LatLng(element.lat,element.lng),
-          title: element.aptName
-        });
-          marker.setMap(map);
-          markerMapping.set(marker,element.aptId)
-          
-          kakao.maps.event.addListener(marker,'click',function(){
-            
-            store.aptId= markerMapping.get(marker)
-         //   console.log(store.aptId);
-            // clickApart(markerMapping[marker])
-          })
-          
-          markers.push(marker)
-          
+    
+    const newMarkerIds = new Set(response.data.map(item => item.aptId));
+    //기존 마커 업데이트
+    markerMapping.forEach((marker, id) => {
+      if (!newMarkerIds.has(id))//기존의 aptId는 사라지지않게하여 다시 렌더링 방지
+      {
+        marker.setMap(null);
+        markerMapping.delete(id);
+      }
+       
+      
+    });
+    
 
+    //apt 각각 Marker에 매핑시켜주기
+    response.data.forEach(element => {
+      if (markerMapping.has(element.aptId)) return;
+      var marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(element.lat, element.lng),
+        title: element.aptName
+      });
+      marker.setMap(map);
+      markerMapping.set(element.aptId, marker);
+     // markerMapping.set(marker, element.aptId)
+      if (map.getLevel() < 4) {
+        kakao.maps.event.addListener(marker, 'click', function () {
+          store.aptId = element.aptId; //markerMapping.get(marker)
+        })
+      }
+     
+      
 
-        });
+    });
 
-    })
+    if (map.getLevel() >= 4) {
+        clusterer.addMarkers(Array.from(markerMapping.values()));
+      }
+
+  });
    
-  
-  
-
+    
 
   
 };
@@ -72,7 +93,7 @@ onMounted(async () => {
     const script = document.createElement("script");
     /* global kakao */
     script.onload = () => kakao.maps.load(initMap);
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${VITE_KAKAO_APP_KEY}`;
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${VITE_KAKAO_APP_KEY}&libraries=services,clusterer,drawing`;
     document.head.appendChild(script);
 
     axios.ge
