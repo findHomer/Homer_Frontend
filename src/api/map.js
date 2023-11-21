f4ec2db21e864bf63e65fd170cf07d5f80792fc4
fangdol888 <fangdol888@gmail.com>
2023-11-21 09:13:04 +0900

    fix : getMarkers return 형식 추가

--- src/api/map.js
+++ src/api/map.js
@@ -3,16 +3,17 @@ import { localAxios } from "@/util/http-commons";
 
 const instance = localAxios()
 
-function getMarkers(bounds, success, fail){
+async function getMarkers(bounds){
     const param ={
         "startLat" : bounds.qa,
         "endLat" : bounds.pa,
         "startLng" :bounds.ha,
         "endLng" :bounds.oa
     }
-    instance.get(`/apartments/locations/maps`, {params : param})
-    .then(success)
-    .catch(fail)
+    const reponse = await instance.get(`/apartments/locations/maps`, {params : param})
+    // .then(success)
+    // .catch(fail)
+    return reponse
 }
 
 export { getMarkers }
