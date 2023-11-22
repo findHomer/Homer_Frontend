2345a42d157dab78453cd3335180d44dd907b6c0
Baek Seungyun <fangdol888@naver.com>
2023-11-22 14:53:59 +0900

    feat : profile 사진 선택 및 프리뷰 업로드 구현

--- src/components/mypage/MypageComponent.vue
+++ src/components/mypage/MypageComponent.vue
@@ -2,17 +2,18 @@
 import { ref, onMounted } from "vue";
 import { getMyInfo } from "@/api/mypage";
 const name = ref(null);
-const email = ref(null);
-const password = ref(null);
 const birth = ref(null);
 const nickname = ref(null);
+const profileUrl = ref("");
+const profile = ref(null);
 
 const getMypage = () => {
   getMyInfo(
     ({ data }) => {
       name.value = data.name;
       birth.value = data.birth;
-      nickname.value = data.nickname
+      nickname.value = data.nickname;
+      profileUrl.value = data.profileUrl;
     },
     (error) => {
       console.log(error);
@@ -23,31 +24,64 @@ const getMypage = () => {
 onMounted(() => {
   getMypage();
 });
+
+const previewImage = () => {
+  profileUrl.value = URL.createObjectURL(profile.value[0]);
+};
+const rules = ref([
+  (value) => {
+    return (
+      !value ||
+      !value.length ||
+      value[0].size < 2000000 ||
+      "Avatar size should be less than 2 MB!"
+    );
+  },
+]);
+
+const changeProfile = () => {
+
+}
 </script>
 
 <template>
   <v-card class="mx-auto" max-width="450" align="center" title="회원 정보">
     <v-container>
-      <v-text-field
-        v-model="email"
-        color="primary"
-        label="이메일"
-        variant="underlined"
-      ></v-text-field>
+      <v-row>
+        <v-col>
+          <v-avatar size="100" color="grey" cover>
+            <v-img
+              width="100"
+              v-if="profileUrl"
+              :src="profileUrl"
+              alt="사용자 프로필 프리뷰"
+              cover
+            />
+          </v-avatar>
+        </v-col>
+      </v-row>
 
-      <v-text-field
-        v-model="password"
-        color="primary"
-        label="비밀번호"
-        placeholder="Enter your password"
-        variant="underlined"
-      ></v-text-field>
+      <v-row>
+        <v-col>
+          <v-file-input
+            :rules="rules"
+            @change="previewImage"
+            v-model="profile"
+            accept="image/png, image/jpeg, image/bmp"
+            placeholder="프로필 사진을 올려주세요"
+            prepend-icon="mdi-camera"
+            label="프로필 업로드"
+          >
+          </v-file-input>
+        </v-col>
+      </v-row>
 
       <v-text-field
         v-model="name"
         color="primary"
         label="이름"
         variant="underlined"
+        readonly
       ></v-text-field>
 
       <v-text-field
@@ -55,6 +89,7 @@ onMounted(() => {
         color="primary"
         label="닉네임"
         variant="underlined"
+        readonly
       ></v-text-field>
 
       <v-text-field
@@ -63,6 +98,7 @@ onMounted(() => {
         label="생일"
         variant="underlined"
         type="date"
+        readonly
       ></v-text-field>
     </v-container>
 
@@ -70,7 +106,7 @@ onMounted(() => {
 
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="success">
+      <v-btn color="success" @click="">
         저장
         <v-icon icon="mdi-chevron-right" end></v-icon>
       </v-btn>
