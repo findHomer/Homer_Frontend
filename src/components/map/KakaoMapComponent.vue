5b87e6b62ee49dc7425cdd25abd04d930c4d65d8
Baek Seungyun <fangdol888@naver.com>
2023-11-22 14:12:51 +0900

    chore : 맵  크기 변경 및 버튼 위치 변경

--- src/components/map/KakaoMapComponent.vue
+++ src/components/map/KakaoMapComponent.vue
@@ -380,7 +380,7 @@ const makePins = function async(response) {
 
 <template>
   <div class="map_wrap">
-  <div id="map" style="width: 100%; height: 100%;position:relative;overflow:hidden;"></div>
+  <div id="map" style="width: 100%; height: 100%; position:relative;overflow:hidden;"></div>
   <ul id="category">
         <li id="BK9" data-order="0"> 
             <span class="category_bg bank"></span>
@@ -414,21 +414,21 @@ const makePins = function async(response) {
 <style scoped>
 
 .map_wrap, .map_wrap * {margin:0; padding:0;font-family:'Malgun Gothic',dotum,'돋움',sans-serif;font-size:12px;}
-.map_wrap {position:relative;width:100vw;height:100vh;}
-#category {position:absolute;top:70px;left:410px;border-radius: 5px; border:1px solid #909090;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);background: #fff;overflow: hidden;z-index: 2;}
+.map_wrap {position:relative;width:100%;height:100%;}
+#category {position:absolute;top:20px;left:20px;border-radius: 5px; border:1px solid #909090;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);background: #fff;overflow: hidden;z-index: 2;}
 #category li {float:left;list-style: none;width:50px;border-right:1px solid #acacac;padding:6px 0;text-align: center; cursor: pointer;}
 #category li.on {background: #eee;}
 #category li:hover {background: #ffe6e6;border-left:1px solid #acacac;margin-left: -1px;}
 #category li:last-child{margin-right:0;border-right:0;}
 #category li span {display: block;margin:0 auto 3px;width:27px;height: 28px;}
 #category li .category_bg {background:url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png) no-repeat;}
-#category li .bank {background-position: -10px 0px;}
-#category li .mart {background-position: -10px -36px;}
-#category li .pharmacy {background-position: -10px -72px;}
-#category li .oil {background-position: -10px -108px;}
-#category li .cafe {background-position: -10px -144px;}
-#category li .store {background-position: -10px -180px;}
-#category li.on .category_bg {background-position-x:-46px;}
+#category li .bank {background-position: -7px 0px;}
+#category li .mart {background-position: -7px -36px;}
+#category li .pharmacy {background-position: -7px -72px;}
+#category li .oil {background-position: -7px -108px;}
+#category li .cafe {background-position: -7px -144px;}
+#category li .store {background-position: -7px -180px;}
+#category li.on .category_bg {background-position-x:-43px;}
 .placeinfo_wrap {position:absolute;bottom:28px;left:-150px;width:300px;}
 .placeinfo {position:relative;width:100%;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;padding-bottom: 10px;background: #fff;}
 .placeinfo:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
